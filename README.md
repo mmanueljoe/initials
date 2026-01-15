2e292718919ea865ace1656e183ff22156b77b74
mmanueljoe <emmanuelletsu18@gmail.com>
2026-01-15 21:53:20 +0000

    chore: update README.md and add sortingAlgorithms.js file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Initials
+# Algorithm design techniques
+
+1. Divide and Conquer (Binary Search, Quick Sort, Merge Sort)
+2. Dynamic Programming (Fibonacci Sequence, Knapsack Problem)
+3. Greedy Algorithm (Dijkstra's Algorithm, Kruskal's Algorithm)
+4. Backtracking (N-Queens Problem, Sudoku Solver)
+5. Branch and Bound (Traveling Salesman Problem)
+6. Randomized Algorithm (Monte Carlo Algorithm, Genetic Algorithm)
+7. Approximation Algorithm (Travelling Salesman Problem)
+8. Heuristic Algorithm (A* Algorithm, Dijkstra's Algorithm)
+9. Meta-heuristic Algorithm (Genetic Algorithm, Particle Swarm Optimization)
+10. Evolutionary Algorithm (Genetic Algorithm, Particle Swarm Optimization)
--- sortingAlgorithms.js
+++ sortingAlgorithms.js
@@ -1,2 +1,105 @@
 // === SORTING ALGORITHMS
 
+// bubble sort algorithm
+function bubbleSort(arr){
+
+    if(arr.length === 0) return [];
+
+    if(arr.length <= 1) return arr;
+
+    let swapped;
+
+    do{
+        swapped = false;
+
+        for(let i = 0; i < arr.length - 1; i++){
+            if(arr[i] > arr[i + 1]){
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while(swapped)
+
+
+    return arr;
+}
+
+console.log(bubbleSort([2,-1,0,6,3,10]))  // [-1, 0, 2, 3, 6, 10]
+console.log(bubbleSort([]))  // []
+console.log(bubbleSort([2]))  // [-1, 0, 2, 3, 6, 10]
+
+
+// insertion sort
+function insertionSort(arr){
+    for(let i = 1; i < arr.length; i++){
+        let numberToInsert = arr[i];
+
+        let j = i - 1; 
+        while(j >= 0 &&  arr[j] > numberToInsert){
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j+1] = numberToInsert;
+    }
+
+    return arr;
+}
+
+
+console.log(insertionSort([-2,6,4,-1,0,7]));
+
+
+
+// quick sort
+function quickSort(arr){
+
+    if(arr.length < 2) return arr;
+
+    let pivot = arr[arr.length - 1];
+
+    let left = [];
+    let right = [];
+
+
+    for(const element of arr){
+        if(element < pivot){
+            left.push(element);
+        }else {
+            right.push(element);
+        }
+    }
+
+    return [...quickSort(left), pivot , ...quickSort(right)];
+}
+
+
+// merge sort
+function mergeSort(arr){
+    if(arr.length < 2) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+
+    const leftArr = arr.slice(0, mid);
+    const rightArr = arr.slice(mid);
+
+    return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(leftArr, rightArr){
+    const sortedArr = [];
+
+    while(leftArr.length && rightArr.length){
+        if(leftArr[0] <= rightArr[0]){
+            sortedArr.push(leftArr.shift());
+        } else {
+            sortedArr.push(rightArr.shift())
+        }
+    }
+
+    return [...sortedArr, ...leftArr, ...rightArr];
+
+}
+
+console.log(mergeSort([2,3,4,-1,10,33,45,22]));
